530978856dd3ea0bf0005c8875c8faa97cffa6da
Brian Wong <brianw385@gmail.com>
2020-03-03 19:55:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,33 +24,6 @@ function divideBy3() {
   number /= 3
 }
 
-divideBy3()
-console.log(number)
-
-add5()
-
-console.log(number)
-
-divideBy3()
-
-console.log(number)
-
-function increment(n) {
-  return n = n += 1
-}
-
-function decrement(n) {
-  return n = n -= 1
-}
-
-function parseInt('3', 10) {
-
-}
-
-function parseFloat(8.444) {
-
-}
-
 function makeInt(string) {
   return parseInt(`string`)
 }
