f1a71316967099229f5b02cff940fab30cae5ddc
Brian Wong <brianw385@gmail.com>
2020-03-03 19:59:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ function divide () {
   4 / 2
 }
 
+function add(a, b) {
+  return a + b
+}
+
+function subtract (a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+
 var number = 10
 
 function add5() {
